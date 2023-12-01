0dea7fe7fed5125996fc40a382c2e1ec23112f62
Cathulhu/Lydia <cathulhu@users.noreply.github.com>
2023-12-01 15:55:04 -0600

    made pythonic changes as request in PR

--- tests/test_api.py
+++ tests/test_api.py
@@ -15,16 +15,18 @@ ferryPort = 8445
 
 # --- test helper functions
 
+@pytest.fixture
 def tokenValidCheck(passedToken):
     if "exp" in passedToken:
         if int(time.time()) < passedToken["exp"]:
             return True;
-    print(" *** Token Failure: Token expired")
-    return False;
+    raise ValueError(" *** Token Failure: Expired")
 
+@pytest.fixture
 def tokenDestroy():
     subprocess.run([tokenDestroyCommand])
 
+@pytest.fixture
 def get_token():
     subprocess.run([TokenGetCommand, "-a", tokenHost, "-i", tokenUser])
 
@@ -34,39 +36,43 @@ def get_token():
     try:
         tokenObject = json.loads(tokenDecoding)
     except ValueError as ve:
-        print(" *** Token Failure: didn't get valid JWT")
-        return False
+        print(" *** Token Failure: Didn't get valid JWT")
+        raise ve
     
     if tokenValidCheck(tokenObject):
         return tokenObject
     else:
-        return False
+        raise ValueError(" *** Token Failure: Expired")
 
+@pytest.fixture
 def getEncodedToken():
     get_token()
-    uid = subprocess.getoutput("id -u")
+    uid = os.getuid()
     encodedToken = ""
     tokenPath = "/run/user/"+uid+"/bt_u"+uid
     with open(tokenPath) as file:
         encodedToken = file.read()
     return encodedToken
 
+@pytest.fixture
 # TODO: replace this with whatever the actual functionality for making calls to the endpoints is in the rest of the program via imports
 def sendToEndpoint(token, endPoint):
     command = 'curl -s -H "Authorization: Bearer '+ token +'" --data-urlencode "setname=hypotana" --get https://ferry.fnal.gov:'+ str(ferryPort) +'/' + endPoint
     try:
         apiResult = json.loads(subprocess.getoutput([command])) 
     except Exception as e:
-        print(" *** API Failure: didn't get valid endpoint response")
+        print(" *** API Failure: Didn't get valid endpoint response")
         return False
     return apiResult
 
 # --- tests below ----
 
+@pytest.mark.integration
 def test_token_aquisition():
     testToken = get_token()
     assert(testToken) is not False
-
+    
+@pytest.mark.integration
 def test_get_capability_set():
     encodedToken = getEncodedToken()
     result = sendToEndpoint(encodedToken, "getCapabilitySet")  
