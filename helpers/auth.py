f06231d551cd9ef6adc67a2eb3041e36d1b5dbd0
Lydia B <cathulhu@users.noreply.github.com>
2023-12-21 13:20:41 -0600

    Update auth.py
    fixed typo, missing " in string

--- helpers/auth.py
+++ helpers/auth.py
@@ -130,7 +130,7 @@ class AuthCert(Auth):
             )
         if not os.path.exists(ca_path):
             raise FileNotFoundError(
-                f"CA dir {ca_path} does not exist. Please check the given path and try again.
+                f"CA dir {ca_path} does not exist. Please check the given path and try again."
             )
         else:
             self.ca_path = ca_path
