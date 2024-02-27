b41b5ed7104e09c797bfd136b3ff6dd99b8e5c42
Shreyas Bhat <sbhat@fnal.gov>
2024-02-27 12:18:14 -0600

    stash_env should actually delete the environment variable temporarily

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,8 @@ import pytest
 def stash_env():
     def inner(env_var):
         env_previous = os.getenv(env_var)
+        if env_previous:
+            del os.environ[env_var]
         yield
         if env_previous:
             os.environ[env_var] = env_previous
