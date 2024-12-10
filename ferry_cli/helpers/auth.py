92ca42502388c69c451c60f9471a206f88bd9720
Shreyas Bhat <sbhat@fnal.gov>
2024-12-10 16:32:49 -0600

    Forgot to remove old --debug option

--- ferry_cli/helpers/auth.py
+++ ferry_cli/helpers/auth.py
@@ -172,13 +172,6 @@ def get_auth_parser() -> "FerryParser":
     auth_parser.add_argument(
         "--ca-path", default=DEFAULT_CA_DIR, help="Certificate authority path"
     )
-    auth_parser.add_argument(
-        "-d",
-        "--debug",
-        action="store_true",
-        default=False,
-        help="Turn on debugging",
-    )
     output_group = auth_parser.add_mutually_exclusive_group(required=False)
     output_group.add_argument(
         "--dryrun",
@@ -186,7 +179,7 @@ def get_auth_parser() -> "FerryParser":
         default=False,
         help="Populate the API call(s) but don't actually run them",
     )
-    auth_parser.add_argument(
+    output_group.add_argument(
         "-d",
         "--debug",
         action="store_true",
